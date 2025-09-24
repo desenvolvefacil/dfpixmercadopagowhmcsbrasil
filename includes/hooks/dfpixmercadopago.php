3d37370477b7721eb8a8aff311a3c1e236538785
Desenvolve Fácil <suporte@desenvolvefacil.com.br>
2025-09-24 19:13:26 -0300

    Create dfpixmercadopago.php

---
+++ includes/hooks/dfpixmercadopago.php
@@ -0,0 +1,74 @@
+<?php
+define('BASE_DIR', dirname(dirname(dirname(__FILE__))) . '/');
+
+if (!defined('WHMCS')) {
+    die();
+}
+
+require_once __DIR__ . '/../../init.php';
+
+use WHMCS\Database\Capsule;
+
+
+// defina o método de pagamento 
+define('PAYMENT_METHOD', 'dfpixmercadopago');
+
+//dfpixmercadopago
+
+
+function dfpixmercadopagocacelarpix($vars)
+{
+    $idfatura = $vars['invoiceid'];
+    
+    $credentials = getGatewayVariables(PAYMENT_METHOD);
+    
+    $access_token =  $credentials['AccessTokenProducao'];
+    
+    
+    
+    //busca o id no banco para cancelar
+     try {
+
+        $fatbd = Capsule::table('dfmercadopagopix')
+                ->select('idfatura', 'idlocationpix', 'pixcopiacola', 'pixqrcode', 'valor')
+                ->where('idfatura', '=', $idfatura)
+                ->get();
+    
+        $IdLocationPix = $fatbd[0]->idlocationpix;
+
+        $url = "https://api.mercadopago.com/v1/payments/".$IdLocationPix;
+    
+        $data = [
+            "status" => "cancelled"
+        ];
+        
+    
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data, JSON_NUMERIC_CHECK));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            "Content-Type: application/json",
+            "Authorization: Bearer $access_token"
+        ]);
+        
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+    
+        $result = json_decode($response, true);
+    
+     } catch (\Exception $e) {
+        
+    }
+    
+    //exclui a fatura do banco de dados
+    Capsule::table('dfmercadopagopix')
+                ->select('idfatura', 'idlocationpix', 'pixcopiacola', 'pixqrcode', 'valor')
+                ->where('idfatura', '=', $idfatura)
+                ->delete();
+
+}
+
+add_hook('InvoiceCancelled', 1, 'dfpixmercadopagocacelarpix');
+
